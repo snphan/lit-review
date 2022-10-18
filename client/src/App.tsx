9613e9797dfcda52e8b11d86f6ed8439f2fadbba
Steven Phan <snphan@ualberta.ca>
2022-10-17 19:49:17 -0600

    start the frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { useState } from 'react';
 
 function App() {
+  const client = new ApolloClient({
+    uri: 'http://localhost:3000/graphql',
+    cache: new InMemoryCache(),
+  });
+
+  const [data, setData] = useState<any>({});
+
+  const getData = () => {
+    client.query({
+      query: gql`
+        query getArticles{
+          articles {
+            id
+            title
+            year
+            summary
+            tags {
+              id
+              name
+            }
+          }
+        }
+      `
+    }).then((result) => setData(result));
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1>Hi World!</h1>
+      <button onClick={() => getData()}>Get Data</button>
+      <div><pre>{JSON.stringify(data, null, 2)}</pre></div>
     </div>
   );
 }
