be3921c2ec9139c2279c57e3f8029c78eb103cf3
Steven Phan <snphan@ualberta.ca>
2022-10-19 20:04:36 -0600

    add filter function my multiple tags

--- api/src/utils/articleLoader.ts
+++ api/src/utils/articleLoader.ts
@@ -6,30 +6,33 @@ import DataLoader from "dataloader";
 
 
 const batchArticles = async (tagIds: number[]) => {
-    const articleTags = await ArticleTag.find({
-        join: {
-            alias: "articleTag",
-            innerJoinAndSelect: {
-                article: "articleTag.article"
-            }
-        },
-        where: {
-            tagId: In(tagIds)
-        } 
-    });
+  console.log('inside of articleloader', tagIds);
+  const articleTags = await ArticleTag.find({
+    join: {
+      alias: "articleTag",
+      innerJoinAndSelect: {
+        article: "articleTag.article"
+      }
+    },
+    where: {
+      tagId: In(tagIds)
+    }
+  });
 
-    const tagIdToArticle: {[key: number]: ArticleEntity[]} = {};
-    
-    articleTags.forEach(at => {
-        const { tagId } = at;
-        if (tagId in tagIdToArticle) {
-            tagIdToArticle[tagId].push((at as any).__article__);
-        } else {
-            tagIdToArticle[tagId] = [(at as any).__article__];
-        }
-    })
+  const tagIdToArticle: { [key: number]: ArticleEntity[] } = {};
 
-    return tagIds.map(tagId => tagIdToArticle[tagId]);
+  articleTags.forEach(at => {
+    const { tagId } = at;
+    if (tagId in tagIdToArticle) {
+      tagIdToArticle[tagId].push((at as any).__article__);
+    } else {
+      tagIdToArticle[tagId] = [(at as any).__article__];
+    }
+  })
+
+  // console.log(tagIdToArticle);
+  console.log(tagIds.map(tagId => tagIdToArticle[tagId]));
+  return tagIds.map(tagId => tagIdToArticle[tagId]);
 }
 
 export const createArticlesLoader = () => new DataLoader(batchArticles);
