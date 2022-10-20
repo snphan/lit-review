f29d9f1143afcf7c8b426a8dfb45ae3838352f76
Steven Phan <snphan@ualberta.ca>
2022-10-19 22:22:33 -0600

    add colors to tags

--- client/src/components/Tag.tsx
+++ client/src/components/Tag.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import React from 'react';
+const randomColor = require('randomcolor');
 
 export interface TagData {
   id: number;
@@ -7,9 +8,8 @@ export interface TagData {
 }
 
 export function Tag(props: TagData) {
-
   return (
-    <div className="tag">
+    <div className="tag" style={{ backgroundColor: randomColor({ luminosity: 'dark', seed: props.id * 167 }) }}>
       {props.name}
     </div>
   )
